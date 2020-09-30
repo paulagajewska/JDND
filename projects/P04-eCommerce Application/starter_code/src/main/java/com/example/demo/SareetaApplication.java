9f7f9364783db46cf7ad8af205eccd18065b0084
gajewskp <paula.gajewska-behnke@vimn.com>
2020-09-30 13:58:47 +0200

    add alert on start application

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/SareetaApplication.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/SareetaApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,12 +9,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.time.LocalDate;
+
+@Slf4j
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SareetaApplication {
 
     public static void main(String[] args) {
+        log.info("Start application on date {}", LocalDate.now());
         SpringApplication.run(SareetaApplication.class, args);
     }
 
